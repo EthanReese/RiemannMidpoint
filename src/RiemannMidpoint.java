efc56f0eed368a3aea126f073b02724c6496798f
EthanReese <ethan.reese01@gmail.com>
2017-06-06 08:49:55 -0700

    Added Comments to provide clarity

--- src/RiemannMidpoint.java
+++ src/RiemannMidpoint.java
@@ -19,9 +19,11 @@ public class RiemannMidpoint {
 
         //Estimate the Arc Length (Part A)
 
-        //Go through every value and add the value at necessary points to the total integral
+        //Go through every value, plug it into the expression, and add the value at necessary points to the total integral
         double totalIntegral = 0;
         for (int i = 0; i < N; i++) {
+            //To find the value to plug in, take the first value, which is the left bound + a small offset and add i * the increment the value increases by
+            //Multiply the value by the delta x (the increment)
             totalIntegral += plugIn((LBOUND + offset) +(increment*i)) * increment;
         }
         System.out.println("The length of the segment is " + totalIntegral);
@@ -64,7 +66,7 @@ public class RiemannMidpoint {
     public double surfaceAreaOfRevolutionPlug(double xVal){
 
         //Change the equation here to meet needs
-        return 2*Math.PI*xVal*Math.sqrt(1+Math.pow((Math.cos(xVal)-Math.sin(xVal))/(2*Math.sqrt(Math.sin(xVal)+Math.cos(xVal))),2));
+        return 2*Math.PI*Math.sqrt(Math.sin(xVal)+Math.cos(xVal))*Math.sqrt(1+Math.pow((Math.cos(xVal)-Math.sin(xVal))/(2*Math.sqrt(Math.sin(xVal)+Math.cos(xVal))),2));
     }
 
     public static void main(String[] args){
